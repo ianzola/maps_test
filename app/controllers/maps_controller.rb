5406bce74f70653a9782a637f3e8f4a57c6f406a
Ingrid Anzola <ingrid@rob01.kns.local>
2010-03-05 16:51:06 +1100

    Setting differents color for polygon

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,9 +5,11 @@ class MapsController < ApplicationController
   def show
     @boundaries = {}
     ssc = %w(11119 11121 11123 11721 11143 11311 11827 11863 11967 12021)
-    ssc.each do |code|
+    stroke_colors = %w(CC3333 009933 FF99FF CC3333 FFCC66 333333 CC9966 CC9999 66FFFF 999933)
+    fill_colors =  %w(99CCFF 00CC33 FFCCFF FF3333 FFFF99 CCCCCC FFCC99 FFCCCC 33CCCC CCCC00)
+    ssc.each_with_index do |code, index|
       url = "public/boundaries/#{code}.json"
-      @boundaries[code] = open(url).read
+      @boundaries.merge!({"#{code}" => {'values' => open(url).read, 'stroke' => stroke_colors[index], 'fill' => fill_colors[index]}}) 
     end
   end
 
